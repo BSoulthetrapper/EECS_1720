4db2b197e9a71c462b6a70f2eed9e29c9baea105
Minkyu(Jonathan) Kim <90640391+BSoulthetrapper@users.noreply.github.com>
2022-01-29 23:04:36 -0500

    10/100 codes

---
+++ sketch_10_100_codes/sketch_10_100_codes.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+}
+
+
+function draw() {
+  rectMode(CORNERS);
+  fill(random(0,255),random(0,255),random(0,255),40);
+  rect(random(0,width), random(0,height), random(0,width), random(0,height));
+}
